765b9b19b6cc6434a5e701a7ec992168685de438
Samuel Gomes <samuelvicosa@gmail.com>
2022-02-01 16:37:18 -0300

    Example Controller class

---
+++ src/main/java/guru/springframework/springboot/controllers/ExampleController.java
@@ -0,0 +1,17 @@
+package guru.springframework.springboot.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+public class ExampleController {
+    @RequestMapping("/")
+    public Map<String, Object> getIndex(){
+        Map<String, Object> returnval = new HashMap<>();
+        returnval.put("hello", "world");
+        return returnval;
+    }
+}
